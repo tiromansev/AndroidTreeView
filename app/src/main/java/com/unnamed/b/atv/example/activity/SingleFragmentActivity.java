8567784798f19e268b7a135263522d521fd2f2b4
tiroman <master_t@inbox.ru>
2017-11-01 16:01:35 +0300

    gradle update

---
+++ app/src/main/java/com/unnamed/b/atv/example/activity/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package com.unnamed.b.atv.example.activity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.unnamed.b.atv.example.R;
+
+/**
+ * Created by Bogdan Melnychuk on 2/12/15.
+ */
+public class SingleFragmentActivity extends AppCompatActivity {
+    public final static String FRAGMENT_PARAM = "fragment";
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_single_fragment);
+
+        Bundle b = getIntent().getExtras();
+        Class<?> fragmentClass = (Class<?>) b.get(FRAGMENT_PARAM);
+        if (bundle == null) {
+            Fragment f = Fragment.instantiate(this, fragmentClass.getName());
+            f.setArguments(b);
+            getFragmentManager().beginTransaction().replace(R.id.fragment, f, fragmentClass.getName()).commit();
+        }
+    }
+}
